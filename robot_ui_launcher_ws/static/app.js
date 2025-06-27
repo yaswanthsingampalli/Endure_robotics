387dcb3f364cfc3d03b187e6ef22c7d2445e1b59
yaswanthsingampalli <yaswanthkumarsingampalli77@gmail.com>
2025-06-28 01:42:38 +0800

    Delete robot_ui_launcher_ws directory

--- robot_ui_launcher_ws/static/app.js
+++
@@ -1,8 +0,0 @@
-//app.js
-function sendCommand(cmd) {
-  fetch(`/${cmd}`)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('status').innerText = `Status: ${data.status}`;
-    });
-}
