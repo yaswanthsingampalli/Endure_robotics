fb82e6c6cc48cedd872dba0432dc0c8c4e04f18d
yaswanthsingampalli <yaswanthkumarsingampalli77@gmail.com>
2025-07-09 01:04:24 +0800

    Delete airport_navigation_ws directory

--- airport_navigation_ws/web_ui/config.js
+++
@@ -1,9 +0,0 @@
-// ✅ config.js
-const CONFIG = {
-  GOOGLE_MAPS_API_KEY: 'AIzaSyD_Xzs073ufiVHqg8QdPMYGfIxnhesA2yM',
-  ROSBRIDGE_SERVER_URL: 'ws://172.27.211.62:9090',
-  DEST_SERVICE: '/destination_node/set_destination',
-  ROUTE_SERVICE: '/route_manager_node/set_route',
-  CURRENT_POS_TOPIC: '/current_position'
-};
-
